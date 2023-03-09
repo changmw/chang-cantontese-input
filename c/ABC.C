60b190e0ce683c3db966acaf67889e4a3bf71a9f
Mr. Man-wai Chang <toylet.toylet@gmail.com>
2023-03-10 02:12:08 +0800

    Add files via upload

---
+++ c/ABC.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+  This program is used to print a PCRAS reports from a given page.
+  A new page is identified by the string "SRH RETAIL AUDIT".
+
+  This function was written to help Data Processing Deparment (of 
+  Survey Reseearch Hongkong) to print a big file from a given page.
+  Back then, the printer might be out of paper. It will be a nightware
+  to restart the whole print job.
+*/
+
+char str[20];
+FILE *p;
+
+void usage();
+
+void main(int argc,char **argv)
+{
+  int i, j, f;
+
+  p = fopen(argv[1],"rb");
+  i = 0;
+  while (fgets(str,1,p) && !kbhit())
+  {
+    if (strncmp(str,"",1)==0) i++;
+	printf("%s pages so far\r");
+  }
+  fclose(p);
+}
---
+++ c/WAITN.C
@@ -0,0 +1,32 @@
+#include <time.h>
+#include <stdlib.h>
+/*
+WAITN - written by Chang Man Wai
+
+Wait for given number of seconds, seconds elapsed will NOT be shown
+- useful in bringing up file/print server after some delay
+*/
+int elapsed_time();
+
+void main(int argc, char *argv[]) {
+  if (argc==1) {
+    printf("\nWAIT v1.0\n");
+    printf("Usage: wait [seconds]\n");
+    exit(1);
+  }
+  while (elapsed_time() < atoi(argv[1])) {
+    if (kbhit()) {
+      bioskey(0);
+      exit(1);
+    }
+  }
+  exit(0);
+}
+
+int elapsed_time() {
+  static int start_time = 0;
+
+  start_time = 0 ? clock()/CLK_TCK : start_time;
+  return(abs(start_time-clock()/CLK_TCK));
+}
+
