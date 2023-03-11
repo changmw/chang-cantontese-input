968ad8d1f97d7210d6b689154fd2b7ff1cc5a0c8
Mr. Man-wai Chang <toylet.toylet@gmail.com>
2023-03-11 13:31:51 +0800

    Update showpkg.ps1

--- powershell/showpkg.ps1
+++ powershell/showpkg.ps1
@@ -1,2 +1,5 @@
+#
+# Command to list packages in Windows 10 matching string "package_name"
+#
 Get-ProvisionedAppxPackage -Online | `
-Where-Object { $_.PackageName -match "xbox" }
+Where-Object { $_.PackageName -match "package_name" }
