b7642f99ce2f1fe47e3581df2ece82a82c82d4a3
Mr. Man-wai Chang <toylet.toylet@gmail.com>
2023-03-11 13:27:44 +0800

    Add files via upload

---
+++ powershell/removexbox.ps1
@@ -0,0 +1,6 @@
+dism /Online /Get-ProvisionedAppxPackages | Select-String PackageName | Select-String xbox
+
+Get-ProvisionedAppxPackage -Online | `
+Where-Object { $_.PackageName -match "xbox" } | `
+ForEach-Object { Remove-ProvisionedAppxPackage -Online -AllUsers -PackageName $_.PackageName }
+
