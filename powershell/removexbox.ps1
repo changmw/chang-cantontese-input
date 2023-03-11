c9b93594dba104780961de77a4d2aaf10e6ff02f
Mr. Man-wai Chang <toylet.toylet@gmail.com>
2023-03-11 13:29:16 +0800

    Update removexbox.ps1

--- powershell/removexbox.ps1
+++ powershell/removexbox.ps1
@@ -1,8 +1,9 @@
-*
-*
-dism /Online /Get-ProvisionedAppxPackages | Select-String PackageName | Select-String xbox
-
+#
+# Commands to remove the pre-installed Xbox app in Windows 10
+#
+#
+# dism /Online /Get-ProvisionedAppxPackages | Select-String PackageName | Select-String xbox
+#
 Get-ProvisionedAppxPackage -Online | `
 Where-Object { $_.PackageName -match "xbox" } | `
 ForEach-Object { Remove-ProvisionedAppxPackage -Online -AllUsers -PackageName $_.PackageName }
-
