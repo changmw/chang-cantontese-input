01f230c51c07d717f7ddaab0e72f06c349fd7a05
Mr. Man-wai Chang <toylet.toylet@gmail.com>
2024-01-26 20:47:25 +0800

    Update get_time.sh

--- dd-wrt/get_time.sh
+++ dd-wrt/get_time.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 # DD-WRT :: View topic - Setting time using HTTP without NTP server nor service
-# https://forum.dd-wrt.com/phpBB2/viewtopic.php?t=327369&highlight=ntp
+# https://forum.dd-wrt.com/phpBB2/viewtopic.php?t=327369
 # 
 curl -I 'https://www.google.com/'  2>/dev/null | grep -i '^date:' | sed 's/^[Dd]ate: //g' | awk 'BEGIN{months="JanFebMarAprMayJunJulAugSepOctNovDec"}{printf("%s%02d%s%s", $4, index(months,$3)/3+1,$2,$5)}' | sed 's/://' | sed 's/:/./'
