67b209e60112b725e48860add1a0ac6b719aaf90
Mr. Man-wai Chang <toylet.toylet@gmail.com>
2024-01-26 20:15:54 +0800

    Create invert.sh

---
+++ dd-wrt/invert.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# reverse a string using awk
+#
+x="welcome"
+
+echo $x | awk \
+'{ for(i = length; i!=0; i--) \
+   x = x substr($0, i, 1); \
+ } END \
+ { print x }'
+
+len=$(echo ${#x})
+while [ $len -ne 0 ]
+do
+  y=$y`echo $x | cut -c $len`
+  let len=len-1
+done
+echo $y
